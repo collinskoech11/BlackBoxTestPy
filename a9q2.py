e86092ed49852ce9e92fadfa27bc3cfaefd3402e
collinskoech11 <collinskoechck34@gmail.com>
2021-03-23 15:32:43 +0300

    q2 update

--- a9q2.py
+++ a9q2.py
@@ -1,5 +1,11 @@
-def closest_to_zero(num1, num2, num3):
+def closest_to_zero(num1, num2, num3, k):
     num1 = 2
     num2 = 7 
-    num3 = 0
+    num3 = 1
+    k = 0
     
+    int_list = [num1, num2, num3]
+
+    return int_list[min(range(len(int_list)), key = lambda i: abs(int_list[i] - k))]
+
+    print(closest_to_zero(int_list, k))
